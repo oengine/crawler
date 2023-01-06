09d51e79b5259d69bb180fdfcabc3b37065b9e04
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-01-06 12:50:45 +0700

    Update crawler.php

--- config/crawler.php
+++ config/crawler.php
@@ -1,10 +1,6 @@
 <?php
 
-use OEngine\Crawler\Commands\CrawlerLink;
-
 return [
-    'name' => '$STUDLY_NAME$',
-    'commands'=>[
-        CrawlerLink::class
-    ]
+    'name' => 'crawler',
+    'commands' => []
 ];
